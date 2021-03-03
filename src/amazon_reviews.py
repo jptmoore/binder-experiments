3ad95dc3dc90fdb7c24c68d5494436f5700a7256
John Moore <jmoore@zedstar.org>
2021-03-03 14:29:40 +0000

    add word cloud support

--- src/amazon_reviews.py
+++ src/amazon_reviews.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import spacy
 from functools import reduce
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+def join_strings(x, y):
+    return x + " " + y
 
 
 def read_chunk(fname, chunk=100, rows=1000):
@@ -16,7 +22,7 @@ def extract_orgs(doc):
 
 def process(chunk):
     for df in chunk:
-        text = reduce(lambda x, y: x + " " + y, df[10])
+        text = reduce(join_strings, df[10])
         doc = nlp(text)
         extract_orgs(doc)
 
@@ -27,3 +33,10 @@ nlp = spacy.load("en_core_web_sm")
 
 chunk = read_chunk("../data/amazon_reviews_sample.csv")
 process(chunk)
+
+text = reduce(join_strings, orgs)
+wordcloud = WordCloud(width=480, height=480, margin=0).generate(text)
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.margins(x=0, y=0)
+plt.show()
